2ce539d67271bfdb60eb72603e5664aa43009438
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-04-02 18:42:44 +0100

    added aliases - very nice!

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-"use strict";
-exports.__esModule = true;
-var person = {
-    name: "Cameron",
-    age: 24,
-    hobbies: ["hockey", "coding", "spinning", "eating"],
-    role: [1, "developer"]
-};
-console.log(person.age);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2, resultType) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    if (resultType === 'as-number') {
+        return parseFloat(result);
+    }
+    else if (resultType === 'as-text') {
+        return result.toString();
+    }
+    else {
+        return result;
+    }
 }
-//note you can use the above because typescript KNOWS WITH CERTAINTY that the hobby will be a string
-//remember a tuple exists in typescript - this means it is of fixed types and length
+var combinedAges = combine(24, 23, 'as-number');
+var combinedNames = combine("Cameron", "Josie", 'as-text');
--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
-function combine(input1: number | string, input2: number | string, resultType: string) {
+export {}
+//alias
+type Combinable = number | string;
+
+function combine(input1: Combinable, input2: Combinable, resultType: string) {
     let result 
     if (typeof input1 === 'number' && typeof input2 === "number"){
      result = input1 + input2;
