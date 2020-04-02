1d8195a1dc087e50cb71221cf044b93f522dda7e
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-04-02 18:37:18 +0100

    union checkers

--- app.ts
+++ app.ts
@@ -1,11 +1,18 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultType: string) {
     let result 
     if (typeof input1 === 'number' && typeof input2 === "number"){
      result = input1 + input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (resultType === 'as-number') {
+    return parseFloat(result);
+    } else if (resultType === 'as-text') {
+        return result.toString()
+    } else {
+        return result;
+    }
 }
 
-const combinedAges = combine(21, 43)
+const combinedAges = combine(24, 23, 'as-number')
+const combinedNames = combine("Cameron", "Josie", 'as-text')
